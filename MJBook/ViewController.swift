0a416f07be4fa97f1a6536fe0dc6e1470fddf422
J1aDong <991383877@qq.com>
2017-10-25 13:20:21 +0800

    Initial Commit

---
+++ MJBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MJBook
+//
+//  Created by J1aDong on 2017/10/25.
+//  Copyright © 2017年 J1aDong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
